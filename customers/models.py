42e0d7d0c56452b7fc18e8427e590d6209b7a1e5
akutsang <akuutsang@gmail.com>
2023-10-08 01:24:10 +0100

    customers.setting

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Customers(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=200)
     industry = models.CharField(max_length=100)
